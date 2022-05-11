c57bf8208562debf7760c558a4eff7ff56f497e6
Vadim Krylov <vadimka.cry.00@gmail.com>
2022-05-11 17:05:38 +0300

    feat: connect to database and create user entity

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,25 +1,26 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { UserEntity } from "./user/entities/user.entity";
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UserModule} from './user/user.module';
+import {UserEntity} from "./user/entities/user.entity";
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: 'localhost',
-    //   port: 5432,
-    //   username: 'postgres',
-    //   password: 'password',
-    //   database: 'tjournal',
-    //   entities: [UserEntity],
-    //   synchronize: true,
-    // }),
-    // UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'tjournal',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UserEntity } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import {Injectable} from '@nestjs/common';
+import {CreateUserDto} from './dto/create-user.dto';
+import {UpdateUserDto} from './dto/update-user.dto';
+import {InjectRepository} from '@nestjs/typeorm';
+import {UserEntity} from './entities/user.entity';
+import {Repository} from 'typeorm';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
-    private repository: Repository<UserEntity>,
-  ) {}
+    private repository: Repository<UserEntity >,
+  ) {
+  }
 
   create(dto: CreateUserDto) {
+    console.log(dto)
     return this.repository.save(dto);
   }
 
